dcf1a4e780b2849f7d271f9643406ffbb73acc16
Jose Morin <carlosmorin78@gmail.com>
2020-11-09 15:32:04 -0600

    comment output examples

--- excercises.rb
+++ excercises.rb
@@ -2,9 +2,10 @@ def get_product(vect)
   vect.reduce :*
 end
 
-puts get_product([1, 4, 21])
-puts get_product([-4, 2.3e12, 77.23, 982, 0b101])
-puts get_product([-3, 11, 2])
-puts get_product([8, 300])
-puts get_product([234, 121, 23, 945, 0])
-puts get_product(1..5)
+# puts get_product([1, 4, 21])
+# puts get_product([-4, 2.3e12, 77.23, 982, 0b101])
+# puts get_product([-3, 11, 2])
+# puts get_product([8, 300])
+# puts get_product([234, 121, 23, 945, 0])
+# puts get_product(1..5)
+
